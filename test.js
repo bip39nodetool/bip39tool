5f3acf9399ab4fc26f4702125b97753733d0f681
Thomas Dahnert <thomas.dahnert@vmzberlin.com>
2025-03-21 21:02:56 +0100

    remove unused files

--- test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var reader = require("readline-sync"); //npm install readline-sync
-var username = reader.question("Username: ");
-var password = reader.question("Password: ", { hideEchoBack: true });
-if (username == "admin" && password == "foobar") {
-    console.log("Welcome!");
-}
